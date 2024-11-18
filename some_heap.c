6a7f0abcefb4f8c028dfdb342c7a205a44e9ff5b
smminassian <sebastienminassian203@gmail.com>
2024-11-17 20:14:33 -0800

    making progress on integer min heap. I just need to get this swap logic down

--- some_heap.c
+++ some_heap.c
@@ -28,13 +28,16 @@ void heap_free(heap_t *heap) {
 
 unsigned int heap_size(heap_t *heap) { return heap->size; }
 
-unsigned int heap_parent(unsigned int index) { return (index - 1) / 2;}
+unsigned int heap_parent(unsigned int index) { 
+    if(index == 0){
+        return -1;
+    }
+    return (index - 1) / 2;
+    }
 
-unsigned int heap_left_child(heap_t *heap, unsigned int index) {
-    if(heap->data[parent])
-}
+unsigned int heap_left_child(unsigned int index) { return 2 * index + 1; }
 
-unsigned int heap_right_child(unsigned int index) {  }
+unsigned int heap_right_child(unsigned int index) {return 2 * index + 2; }
 
 unsigned int heap_level(unsigned int index) {}
 
@@ -46,10 +49,23 @@ void heap_print(heap_t *heap) {
     printf("\n");
 }
 
-void heap_swap(heap_t *heap, int index1, int index2) {
+void heap_swap(heap_t *heap, int index1, int index2, unsigned int index) {
+    index1 = heap_left_child(index);
+    index2 = heap_right_child(index);
+    int parent = heap_parent(index);
+
+    if(index1 < 0){
+        index1 = -1;
+    }
+    else if(index2 < 0){
+        index2 = -1;
+    }
+
+    if(index1 != -1 && heap->data[index1] < heap->data[parent])
 }
 
 void heap_bubble_up(heap_t *heap, int index) {
+    
     while(index > 0 && heap->data[index/2] > ){
 
     }
